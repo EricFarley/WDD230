5662e9e3d9d40fddfdb93222a8b531d7c77e1e54
Eric Farley <ericfarley96@gmail.com>
2022-10-26 23:25:04 -0300

    Added lesson 7

---
+++ lesson7/scripts/main.js
@@ -0,0 +1,27 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+if ("IntersectionObserver" in window) {
+    const imgOptions = { threshold: 1 };
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
